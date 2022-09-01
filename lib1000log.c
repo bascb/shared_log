1038ae80b427732a7b9c39067d1c0b3e51e62c5f
Bernardo <bercbranco@gmail.com>
2022-09-01 11:25:01 +0000

    Using 42 norminette to format source code. And adding my header

--- lib1000log.c
+++ lib1000log.c
@@ -1,4 +1,15 @@
-/* Implementation for log parameters and functions */
+/**********************************************************
+*                                                         *
+* lib1000log.c                                            *
+*                                                         *
+* Description:                                            *
+*  Implementation of a simple module to log to small      *
+*  files. Max 1000 lines per file                         *
+*                                                         *
+* Bernardo Castelo Branco                                 *   
+* https://github.com/bascb                                *
+*                                                         * 
+**********************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +22,7 @@
 ****************************************************/
 
 /* Returns pointer to the right file */
-FILE *Log_Set_File(char *PathToLogFolder, char *Type);
+FILE	*Log_Set_File(char *PathToLogFolder, char *Type);
 
 /* Format time string 
 *
@@ -20,25 +31,26 @@ FILE *Log_Set_File(char *PathToLogFolder, char *Type);
 *   p - Print to log entry
 *
 */
-void Log_FormatTime(char Type, char *String);
+void	Log_FormatTime(char Type, char *String);
 
 /* Count number of file lines */
-int Log_Count_File_Lines(char *Path);
+int		Log_Count_File_Lines(char *Path);
 
 /****************************************************
 	Implementation of public functions
 ****************************************************/
 
 /* Add an entry to log file */
-void Log_Entry(char *PathToLogFolder,char *Type, char *Message) {
-	FILE *F;
-	char TimeString[200];
-	
-	F = Log_Set_File(PathToLogFolder,Type);
+void	Log_Entry(char *PathToLogFolder, char *Type, char *Message)
+{
+	FILE	*F;
+	char	TimeString[200];
 
-	Log_FormatTime('p',TimeString);
+	F = Log_Set_File(PathToLogFolder, Type);
 
-	fprintf(F,"%s : %s\n",TimeString,Message);
+	Log_FormatTime('p', TimeString);
+
+	fprintf(F, "%s : %s\n", TimeString, Message);
 	fclose(F);
 }
 
@@ -47,44 +59,49 @@ void Log_Entry(char *PathToLogFolder,char *Type, char *Message) {
 ****************************************************/
 
 /* Returns pointer to the right file */
-FILE *Log_Set_File(char *PathToLogFolder, char *Type) {
-	FILE *F;
-	char TimeString[200];
-	char *Path = NULL, *Old = NULL;
-	int l;
+FILE	*Log_Set_File(char *PathToLogFolder, char *Type)
+{
+	FILE	*F;
+	char	TimeString[200];
+	char	*Path = NULL, *Old = NULL;
+	int		l;
 
-	Path = (char *)calloc(strlen(PathToLogFolder)+strlen(Type)+10,sizeof(char));
-	if ( Path == NULL ) return NULL;
+	Path = (char *)calloc(strlen(PathToLogFolder) + strlen(Type) + 10, sizeof(char));
+	if (Path == NULL)
+		return (NULL);
 
-	strcpy(Path,PathToLogFolder);
+	strcpy(Path, PathToLogFolder);
 	strcat(Path, "/");
 	strcat(Path, Type);
 	strcat(Path, ".log");
 
-	F = fopen(Path,"r");
-	if ( F != NULL ) {
+	F = fopen(Path, "r");
+	if (F != NULL) 
+	{
 		l = Log_Count_File_Lines(Path);
 		fclose(F);
-		if ( l > 999 ) {
-			Old = (char *)calloc(strlen(PathToLogFolder)+strlen(Type)+210,sizeof(char));
-			if ( Old == NULL ) {
+		if (l > 999)
+		{
+			Old = (char *)calloc(strlen(PathToLogFolder) + strlen(Type) + 210, sizeof(char));
+			if (Old == NULL)
+			{
 				free(Path);
-				return NULL;
+				return (NULL);
 			}
-			strcpy(Old,PathToLogFolder);
+			strcpy(Old, PathToLogFolder);
 			strcat(Old, "/");
 			strcat(Old, Type);
-			Log_FormatTime('a',TimeString);
-			strcat(Old,TimeString);
-			strcat(Old,".log");
-			rename(Path,Old);
+			Log_FormatTime('a', TimeString);
+			strcat(Old, TimeString);
+			strcat(Old, ".log");
+			rename(Path, Old);
 			free(Old);
 		}
 	}
-	F = fopen(Path,"a");
+	F = fopen(Path, "a");
 	free(Path);
 
-	return F;
+	return (F);
 }
 
 /* Format time string 
@@ -94,116 +111,142 @@ FILE *Log_Set_File(char *PathToLogFolder, char *Type) {
 *   p - Print to log entry
 *
 */
-void Log_FormatTime(char Type, char *String) {
-	time_t Now;
-	struct tm *NowInfo;
-	char Str[100];
+void	Log_FormatTime(char Type, char *String)
+{
+	time_t		Now;
+	struct tm	*NowInfo;
+	char		Str[100];
 
 	time(&Now);
 	NowInfo = localtime(&Now);
 
-	if ( Type == 'p' ) {
-		sprintf(Str,"%d-",NowInfo->tm_year+1900);
-		strcpy(String,Str);
-		if ( NowInfo->tm_mon + 1 < 10 ) {
-			sprintf(Str,"0%d-",NowInfo->tm_mon + 1);
-			strcat(String,Str);
+	if (Type == 'p')
+	{
+		sprintf(Str, "%d-", NowInfo->tm_year + 1900);
+		strcpy(String, Str);
+		if (NowInfo->tm_mon + 1 < 10)
+		{
+			sprintf(Str, "0%d-", NowInfo->tm_mon + 1);
+			strcat(String, Str);
 		}
-		else {
-			sprintf(Str,"%d-",NowInfo->tm_mon + 1);
-			strcat(String,Str);
+		else
+		{
+			sprintf(Str, "%d-", NowInfo->tm_mon + 1);
+			strcat(String, Str);
 		}
-		if ( NowInfo->tm_mday  < 10 ) {
-			sprintf(Str,"0%d ",NowInfo->tm_mday);
-			strcat(String,Str);
+		if (NowInfo->tm_mday  < 10)
+		{
+			sprintf(Str, "0%d ", NowInfo->tm_mday);
+			strcat(String, Str);
 		}
-		else {
-			sprintf(Str,"%d ",NowInfo->tm_mday);
-			strcat(String,Str);
+		else
+		{
+			sprintf(Str, "%d ", NowInfo->tm_mday);
+			strcat(String, Str);
 		}
-		if ( NowInfo->tm_hour  < 10 ) {
-			sprintf(Str,"0%d:",NowInfo->tm_hour);
-			strcat(String,Str);
+		if (NowInfo->tm_hour  < 10)
+		{
+			sprintf(Str, "0%d:", NowInfo->tm_hour);
+			strcat(String, Str);
 		}
-		else {
-			sprintf(Str,"%d:",NowInfo->tm_hour);
-			strcat(String,Str);
+		else
+		{
+			sprintf(Str, "%d:", NowInfo->tm_hour);
+			strcat(String, Str);
 		}
-		if ( NowInfo->tm_min  < 10 ) {
-			sprintf(Str,"0%d:",NowInfo->tm_min);
-			strcat(String,Str);
+		if (NowInfo->tm_min  < 10)
+		{
+			sprintf(Str, "0%d:", NowInfo->tm_min);
+			strcat( String, Str);
 		}
-		else {
-			sprintf(Str,"%d:",NowInfo->tm_min);
-			strcat(String,Str);
+		else
+		{
+			sprintf(Str, "%d:", NowInfo->tm_min);
+			strcat(String, Str);
 		}
-		if ( NowInfo->tm_sec  < 10 ) {
-			sprintf(Str,"0%d",NowInfo->tm_sec);
-			strcat(String,Str);
+		if (NowInfo->tm_sec  < 10)
+		{
+			sprintf(Str, "0%d", NowInfo->tm_sec);
+			strcat(String, Str);
 		}
-		else {
-			sprintf(Str,"%d",NowInfo->tm_sec);
-			strcat(String,Str);
+		else
+		{
+			sprintf(Str, "%d", NowInfo->tm_sec);
+			strcat(String, Str);
 		}
 	}
-	else if ( Type == 'a' ) {
-		sprintf(Str,"%d",NowInfo->tm_year+1900);
-		strcpy(String,Str);
-		if ( NowInfo->tm_mon + 1 < 10 ) {
-			sprintf(Str,"0%d",NowInfo->tm_mon + 1);
-			strcat(String,Str);
+	else if (Type == 'a')
+	{
+		sprintf(Str, "%d", NowInfo->tm_year+1900);
+		strcpy(String, Str);
+		if (NowInfo->tm_mon + 1 < 10)
+		{
+			sprintf(Str, "0%d", NowInfo->tm_mon + 1);
+			strcat(String, Str);
 		}
-		else {
-			sprintf(Str,"%d",NowInfo->tm_mon + 1);
-			strcat(String,Str);
+		else
+		{
+			sprintf(Str, "%d", NowInfo->tm_mon + 1);
+			strcat(String, Str);
 		}
-		if ( NowInfo->tm_mday  < 10 ) {
-			sprintf(Str,"0%d",NowInfo->tm_mday);
-			strcat(String,Str);
+		if (NowInfo->tm_mday  < 10)
+		{
+			sprintf(Str, "0%d", NowInfo->tm_mday);
+			strcat(String, Str);
 		}
-		else {
-			sprintf(Str,"%d",NowInfo->tm_mday);
-			strcat(String,Str);
+		else
+		{
+			sprintf(Str, "%d", NowInfo->tm_mday);
+			strcat(String, Str);
 		}
-		if ( NowInfo->tm_hour  < 10 ) {
-			sprintf(Str,"0%d",NowInfo->tm_hour);
-			strcat(String,Str);
+		if (NowInfo->tm_hour  < 10)
+		{
+			sprintf(Str, "0%d", NowInfo->tm_hour);
+			strcat(String, Str);
 		}
-		else {
-			sprintf(Str,"%d",NowInfo->tm_hour);
-			strcat(String,Str);
+		else
+		{
+			sprintf(Str,"%d", NowInfo->tm_hour);
+			strcat(String, Str);
 		}
-		if ( NowInfo->tm_min  < 10 ) {
-			sprintf(Str,"0%d",NowInfo->tm_min);
-			strcat(String,Str);
+		if (NowInfo->tm_min  < 10)
+		{
+			sprintf(Str, "0%d", NowInfo->tm_min);
+			strcat(String, Str);
 		}
-		else {
-			sprintf(Str,"%d",NowInfo->tm_min);
-			strcat(String,Str);
+		else
+		{
+			sprintf(Str, "%d", NowInfo->tm_min);
+			strcat(String, Str);
 		}
-		if ( NowInfo->tm_sec  < 10 ) {
-			sprintf(Str,"0%d",NowInfo->tm_sec);
-			strcat(String,Str);
+		if (NowInfo->tm_sec  < 10)
+
+		{
+			sprintf(Str, "0%d", NowInfo->tm_sec);
+			strcat(String, Str);
 		}
-		else {
-			sprintf(Str,"%d",NowInfo->tm_sec);
-			strcat(String,Str);
+		else
+		{
+			sprintf(Str, "%d", NowInfo->tm_sec);
+			strcat(String, Str);
 		}
 	}
 }
 
 /* Count number of file lines */
-int Log_Count_File_Lines(char *Path) {
+int		Log_Count_File_Lines(char *Path)
+{
 	int Lines = 0;
 	FILE *F;
 	char c;
 
 	F = fopen(Path,"r");
-	if ( F == NULL ) return 0;
+	if (F == NULL) 
+		return (0);
 
-	while ( (c = fgetc(F) ) != EOF ) if ( c == '\n' ) Lines++;
+	while ((c = fgetc(F)) != EOF) if (c == '\n') Lines++;
 
 	fclose(F);
 
-	return Lines;
+	return (Lines);
 }
--- lib1000log.h
+++ lib1000log.h
@@ -1,8 +1,20 @@
-/* Interface for log parameters and functions */
-#ifndef lib1000log_h__
-#define lib1000log_h__
+/**********************************************************
+*                                                         *
+* lib1000log.h                                            *
+*                                                         *
+* Description:                                            *
+*  Interface of a simple module to log to small files     *
+*  Max 1000 lines per file                                *
+*                                                         *
+* Bernardo Castelo Branco                                 *   
+* https://github.com/bascb                                *
+*                                                         * 
+**********************************************************/
+
+#ifndef LIB1000LOG_H
+# define LIB1000LOG_H
 
 /* Add an entry to log file */
-extern void Log_Entry(char *PathToLogFolder,char *Type,char *Message);
+extern void	Log_Entry(char *PathToLogFolder, char *Type, char *Message);
 
 #endif
